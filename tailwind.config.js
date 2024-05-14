f278a36e3846356cc55846165cd20f30007d2f7a
Alireza mohammadi <alireza.mohammadi.m19@gmail.com>
2024-05-14 16:40:26 +0330

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    fontFamily: {
+      sans: ["Vazir,Vazir-Bold"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
